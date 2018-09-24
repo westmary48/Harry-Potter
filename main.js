0995afd1479a9b6f9620cb896a9a708679ab7244
Mary West <westmarye1219@gmail.com>
2018-09-23 19:04:56 -0500

    sunday

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
+let house  = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin']
+
 const studentInputElm = document.getElementById('studentInputElm');
 // const letsStartSortingElm = document.getElementById('letsStartSortingElm');
 
 const sortButtonElm = document.getElementById('sortButton');
 const letsStartSortingElm = document.getElementById('letsStartSortingButton');
 
-
-// document.getElementById("sortButton").addEventListener("click", function (e) {
-//     studentInputElm.innerHTML = `you clicked the button'"$(e.target.id}"`;
-// });
 const activateDeletes = () => {
     const deleteButtons = document.getElementsByClassName('deleteButton');
-    console.log(deleteButtons);
 
     for (let i = 0; i < deleteButtons.length; i++) {
         const element = deleteButtons[i];
@@ -18,7 +15,6 @@ const activateDeletes = () => {
             const buttonIClicked = e.target;
             const cardToDelete = buttonIClicked.parentNode.parentNode;
             cardToDelete.remove();
-            console.log("they clicked delete!!");
         })
     }
 
@@ -29,15 +25,17 @@ const printToDom = (stringToPrint, whereToPrint) => {
 
 }
     const buildNewStudentCard = (sort) => {
+        let newHouse = Math.floor((Math.random()*house.length));
         let domString = `<div class="card w-25 m-2">
         <div class ="card-body">
         <h5 class="card-title">${sort}</h5>
-        <button href="#" class="btn btn-danger ">You got Ravenclaw!</button>
+        <p> Your new house is... ${house[newHouse]}</p>
+        <button href="#" class="btn btn-danger deleteButton">Delete!</button>
       </div>
       </div>`;
 
         printToDom(domString, 'wizzardCard');
-        activatesDeletes();
+        activateDeletes();
     }
 
     sortButtonElm.addEventListener("click", (e) => {
